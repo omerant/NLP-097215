82f08e9fa21cb1603b831734e0a1cd29f48c4d5c
omerant <omer.antverg1@gmail.com>
2020-04-24 12:28:53 +0300

    Add files via upload

---
+++ constant_tag_word_sets.py
@@ -0,0 +1,35 @@
+class WordConstantTagSets:
+    CC_set = {'and', 'or', 'but', '&'}#
+    CD_set = {'million', 'billion'}#
+    DT_set = {'the', 'a', 'this', 'these', 'an'}#
+    IN_set = {'for', 'at', 'from', 'than', 'if', 'into', 'under'}#
+    JJ_set = {'other'}#
+    LRB_set = {'-LRB-'}#
+    MD_set = {'would', 'could', 'may', 'can'}#
+    NN_set = {'year', 'business', 'week'}#
+    NNP_set = {'Mr.', 'Corp.', 'U.S.', 'mr.', 'New', 'Co.', 'York', 'Friday'}#
+    NNS_set = {'years', 'companies'}#
+    PRP_set = {'it', 'he', 'they'}#
+    PRP_DOLLAR_set = {'its', 'their'}#
+    RB_set = {'not', 'also', 'now'}#
+    RRB_set = {'-RRB-'}#
+    TO_set = {'to'}#
+    VBD_set = {'was', 'were'}#
+    VBN_set = {'been'}#
+    VBZ_set = {'is', 'says'}#
+    VB_set = {'be'}#
+
+
+    WDT_set = {'which'}#
+    WP_set = {'who'}#
+    WRB_set = {'when'}#
+
+
+
+    COMMA_set = {','}#
+    APOSTROPHES_set = {'\'\''}
+    PERCENT_set = {'%'}#
+    COLON_set = {':', ';', '--'}#
+    QUESTION_MARK_set = {'?'}#
+    BACKTICKS_set = {'``'}#
+    DOLLAR_set = {'$'}#
