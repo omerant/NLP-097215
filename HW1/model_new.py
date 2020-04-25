f6966059570a0bb956fd47593d25a42f93f69a53
micha <miken081990@gmail.com>
2020-04-25 11:19:44 +0300

    add method to load v, add main to run viterbi

--- HW1/model_new.py
+++ HW1/model_new.py
@@ -5,6 +5,7 @@ from pre_processing import FeatureStatistics
 from features import History
 from scipy.optimize import fmin_l_bfgs_b
 from utils import timeit
+from viterbi import Viterbi
 
 
 class MaximumEntropyMarkovModel:
@@ -14,12 +15,13 @@ class MaximumEntropyMarkovModel:
         self.dump_weights_path = 'weights'
         self.prob_dict = dict()
 
-    def load_v_from_pickle(self):
-        print('filling probability dict')
-        weights_dir = os.path.join(self.dump_weights_path, str(self.feature_statistics.version) +
-                                   f'-reg_lambda={0.1}-num_iter={1}')
+    @staticmethod
+    def load_v_from_pickle(dump_weights_path, version):
+        weights_dir = os.path.join(dump_weights_path, str(version) +
+                                   f'-reg_lambda={0.01}-num_iter={1}')
         with open(weights_dir, 'rb') as f:
-            self.v = pickle.load(f).reshape(-1)
+            v = pickle.load(f).reshape(-1)
+        return v
 
     def fill_prob_dict(self):
         print('filling probability dict')
@@ -156,19 +158,32 @@ class MaximumEntropyMarkovModel:
 
 
 if __name__ == '__main__':
+    # TRAIN MODEL
+    # train1_path = 'data/train1.wtag'
+    # test1_short_path = 'data/test1.wtag'
+    # memm = MaximumEntropyMarkovModel(train_data_path=train1_path)
+    # memm.optimize_model()
+
+    # RUN VITERBI
+    v = MaximumEntropyMarkovModel.load_v_from_pickle(dump_weights_path='weights', version=1)
     train1_path = 'data/train1.wtag'
-    test1_short_path = 'data/test1.wtag'
-    memm = MaximumEntropyMarkovModel(train_data_path=train1_path)
-    memm.optimize_model()
-    # memm.load_v_from_pickle()
-    # memm.fill_prob_dict()
-    # viterbi = Viterbi(
-    #     v=memm.v,
-    #     sentence_hist_list=memm.test_data.history_sentence_list,
-    #     tags_set=memm.feature_statistics.tags_set,
-    #     all_possible_tags_dict=memm.feature_statistics.all_possible_tags_dict,
-    #     get_feature_from_hist=memm.feature_statistics.get_non_zero_sparse_feature_vec_indices_from_history
-    # )
+    test1_path = 'data/test1.wtag'
+    ft_statistics = FeatureStatistics(input_file_path=train1_path)
+    ft_statistics.pre_process(fill_possible_tag_dict=False)
+    test_sentence_hist_list = FeatureStatistics.fill_ordered_history_list(file_path=test1_path)
+    tag_set = ft_statistics.tags_set
+    all_possible_tags_dict = ft_statistics.all_possible_tags_dict
+    get_ft_from_hist_func = ft_statistics.get_non_zero_sparse_feature_vec_indices_from_history
+    word_possible_tag_set = ft_statistics.word_possible_tag_set
+    word_possible_tag_with_threshold_dict = ft_statistics.word_possible_tag_with_threshold_dict
+
+    viterbi = Viterbi(
+        v=v, sentence_hist_list=test_sentence_hist_list, tags_set=tag_set,
+        all_possible_tags_dict=all_possible_tags_dict, get_feature_from_hist=get_ft_from_hist_func,
+        word_possible_tag_set=word_possible_tag_set,
+        word_possible_tag_with_threshold_dict=word_possible_tag_with_threshold_dict
+    )
+    viterbi.predict_all_test()
     # all_res_list, all_acc_list = viterbi.predict_all()
     # # print(f'avg acc: {sum(all_acc_list)/len(all_acc_list)}')
     # stop = timeit.default_timer()
