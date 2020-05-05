7b0a87fd27143d84ed91d906e9f8ee574aabf50d
micha <miken081990@gmail.com>
2020-05-05 16:45:32 +0300

    update

--- HW1/config.py
+++ HW1/config.py
@@ -30,4 +30,32 @@ class BaseCfg(Cfg):
         ft.ContainsSymbolDict,
         ft.ContainsOnlySymbolsDict,
         ft.TwoPreviousTagsAndCurrentWord,
+        ft.NextWordCommasDict
+    }
+
+
+class NoPrefSufCfg(Cfg):
+    feature_dicts = {
+        # fill dict for 1-3 ngrams
+        ft.TrigramTagsCountDict,
+        ft.BigramTagsCountDict,
+        ft.UnigramTagsCountDict,
+        ft.WordsTagsCountDict,
+        # ft.WordsPrefixTagsCountDict,
+        # ft.WordsSuffixTagsCountDict,
+        ft.PrevWordCurrTagCountDict,
+        ft.NextWordCurrTagCountDict,
+        ft.DoublePrevWordCurrTagCountDict,
+        ft.DoubleNextWordCurrTagCountDict,
+        ft.SkipBigramCountDict,
+        # letters digits
+        ft.HasFirstCapitalLetterDict,
+        ft.HasAllCapitalLettersDict,
+        ft.HasDigitDict,
+        ft.HasOnlyDigitDict,
+        ft.ContainsHyphenDict,
+        ft.IsFirstWordDict,
+        ft.ContainsSymbolDict,
+        ft.ContainsOnlySymbolsDict,
+        ft.TwoPreviousTagsAndCurrentWord,
     }
