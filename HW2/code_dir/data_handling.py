cff4dd3d04c354267a51e5000d5603de9cc9bc4a
micha <miken081990@gmail.com>
2020-05-28 12:38:30 +0300

    add hw2 files

---
+++ HW2/code_dir/data_handling.py
@@ -0,0 +1,116 @@
+from collections import defaultdict
+import torch
+from utils import split
+from torchtext.vocab import Vocab
+from torch.utils.data.dataset import Dataset, TensorDataset
+from pathlib import Path
+from collections import Counter
+from constants import PAD_TOKEN, SPECIAL_TOKENS, UNKNOWN_TOKEN
+import os.path as osp
+
+
+class PosDataReader:
+    def __init__(self, file, word_dict, pos_dict):
+        self.file = file
+        self.word_dict = word_dict
+        self.pos_dict = pos_dict
+        self.sentences = []
+        self.__readData__()
+
+    def __readData__(self):
+        """main reader function which also populates the class data structures"""
+        with open(self.file, 'r') as f:
+            for line in f:
+                cur_sentence = []
+                splited_words = split(line, (' ', '\n'))
+                del splited_words[-1]
+                for word_and_tag in splited_words:
+                    cur_word, cur_tag = split(word_and_tag, '_')
+                    cur_sentence.append((cur_word, cur_tag))
+                self.sentences.append(cur_sentence)
+
+    def get_num_sentences(self):
+        """returns num of sentences in data"""
+        return len(self.sentences)
+
+
+class PosDataset(Dataset):
+    def __init__(self, word_dict, pos_dict, dir_path: str, subset: str,
+                 padding=False, word_embeddings=None):
+        super().__init__()
+        self.subset = subset  # One of the following: [train, test]
+        self.file = osp.join(dir_path, subset + ".wtag")
+        self.datareader = PosDataReader(self.file, word_dict, pos_dict)
+        self.vocab_size = len(self.datareader.word_dict)
+        if word_embeddings:
+            self.word_idx_mappings, self.idx_word_mappings, self.word_vectors = word_embeddings
+        else:
+            self.word_idx_mappings, self.idx_word_mappings, self.word_vectors = self.init_word_embeddings(
+                self.datareader.word_dict)
+        self.pos_idx_mappings, self.idx_pos_mappings = self.init_pos_vocab(self.datareader.pos_dict)
+
+        self.pad_idx = self.word_idx_mappings.get(PAD_TOKEN)
+        self.unknown_idx = self.word_idx_mappings.get(UNKNOWN_TOKEN)
+        self.word_vector_dim = self.word_vectors.size(-1)
+        self.sentence_lens = [len(sentence) for sentence in self.datareader.sentences]
+        self.max_seq_len = max(self.sentence_lens)
+        self.sentences_dataset = self.convert_sentences_to_dataset(padding)
+
+    def __len__(self):
+        return len(self.sentences_dataset)
+
+    def __getitem__(self, index):
+        word_embed_idx, pos_embed_idx, sentence_len = self.sentences_dataset[index]
+        return word_embed_idx, pos_embed_idx, sentence_len
+
+    @staticmethod
+    def init_word_embeddings(word_dict):
+        glove = Vocab(Counter(word_dict), vectors="glove.6B.300d", specials=SPECIAL_TOKENS)
+        return glove.stoi, glove.itos, glove.vectors
+
+    def get_word_embeddings(self):
+        return self.word_idx_mappings, self.idx_word_mappings, self.word_vectors
+
+    def init_pos_vocab(self, pos_dict):
+        idx_pos_mappings = sorted([self.word_idx_mappings.get(token) for token in SPECIAL_TOKENS])
+        pos_idx_mappings = {self.idx_word_mappings[idx]: idx for idx in idx_pos_mappings}
+
+        for i, pos in enumerate(sorted(pos_dict.keys())):
+            # pos_idx_mappings[str(pos)] = int(i)
+            pos_idx_mappings[str(pos)] = int(i + len(SPECIAL_TOKENS))
+            idx_pos_mappings.append(str(pos))
+        print("idx_pos_mappings -", idx_pos_mappings)
+        print("pos_idx_mappings -", pos_idx_mappings)
+        return pos_idx_mappings, idx_pos_mappings
+
+    def get_pos_vocab(self):
+        return self.pos_idx_mappings, self.idx_pos_mappings
+
+    def convert_sentences_to_dataset(self, padding):
+        sentence_word_idx_list = list()
+        sentence_pos_idx_list = list()
+        sentence_len_list = list()
+        for sentence_idx, sentence in enumerate(self.datareader.sentences):
+            words_idx_list = []
+            pos_idx_list = []
+            for word, pos in sentence:
+                words_idx_list.append(self.word_idx_mappings.get(word))
+                pos_idx_list.append(self.pos_idx_mappings.get(pos))
+            sentence_len = len(words_idx_list)
+            # if padding:
+            #     while len(words_idx_list) < self.max_seq_len:
+            #         words_idx_list.append(self.word_idx_mappings.get(PAD_TOKEN))
+            #         pos_idx_list.append(self.pos_idx_mappings.get(PAD_TOKEN))
+            sentence_word_idx_list.append(torch.tensor(words_idx_list, dtype=torch.long, requires_grad=False))
+            sentence_pos_idx_list.append(torch.tensor(pos_idx_list, dtype=torch.long, requires_grad=False))
+            sentence_len_list.append(sentence_len)
+
+        # if padding:
+        #     all_sentence_word_idx = torch.tensor(sentence_word_idx_list, dtype=torch.long)
+        #     all_sentence_pos_idx = torch.tensor(sentence_pos_idx_list, dtype=torch.long)
+        #     all_sentence_len = torch.tensor(sentence_len_list, dtype=torch.long, requires_grad=False)
+        #     return TensorDataset(all_sentence_word_idx, all_sentence_pos_idx, all_sentence_len)
+
+        return {i: sample_tuple for i, sample_tuple in enumerate(zip(sentence_word_idx_list,
+                                                                     sentence_pos_idx_list,
+                                                                     sentence_len_list))}
---
+++ HW2/code_dir/utils.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+
+
+def split(string, delimiters):
+    """
+        Split strings according to delimiters
+        :param string: full sentence
+        :param delimiters string: characters for spliting
+            function splits sentence to words
+    """
+    delimiters = tuple(delimiters)
+    stack = [string, ]
+
+    for delimiter in delimiters:
+        for i, substring in enumerate(stack):
+            substack = substring.split(delimiter)
+            stack.pop(i)
+            for j, _substring in enumerate(substack):
+                stack.insert(i + j, _substring)
+
+    return stack
+
+
+def get_vocabs(list_of_paths):
+    """
+        Extract vocabs from given datasets. Return a word2ids and tag2idx.
+        :param file_paths: a list with a full path for all corpuses
+            Return:
+              - word2idx
+              - tag2idx
+    """
+    word_dict = defaultdict(int)
+    pos_dict = defaultdict(int)
+    for file_path in list_of_paths:
+        with open(file_path) as f:
+            for line in f:
+                splited_words = split(line, (' ', '\n'))
+                del splited_words[-1]
+                for word_and_tag in splited_words:
+                    word, pos_tag = split(word_and_tag, '_')
+                    word_dict[word] += 1
+                    pos_dict[pos_tag] += 1
+
+    return word_dict, pos_dict
+
