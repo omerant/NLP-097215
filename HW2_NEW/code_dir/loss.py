a11bedf880e0751ceb0eef1408ea5034fc683d72
micha <miken081990@gmail.com>
2020-06-04 11:36:40 +0300

    receive log prob tensor in loss

--- HW2_NEW/code_dir/loss.py
+++ HW2_NEW/code_dir/loss.py
@@ -7,7 +7,7 @@ class NllLoss(torch.nn.Module):
     def __init__(self):
         super(NllLoss, self).__init__()
 
-    def forward(self, y_true, y_pred) -> torch.Tensor:
+    def forward(self, log_prob_y_pred, y_pred) -> torch.Tensor:
         """
 
         :param y_true:
@@ -15,20 +15,21 @@ class NllLoss(torch.nn.Module):
         :return:
         """
         Y_i_size = y_true.shape[1]
-        prob_y_pred = F.softmax(y_pred, 2)
-        our_loss = -(1. / (Y_i_size * y_true.shape[0])) * torch.sum(torch.log(prob_y_pred.gather(1, y_true.unsqueeze(1))))
+        our_loss = -(1. / (Y_i_size * y_true.shape[0])) * torch.sum(log_prob_y_pred.gather(1, y_true.unsqueeze(1)))
         return our_loss
 
 
 if __name__ == '__main__':
 
     # test loss with 3d tensor
-    y_pred = torch.tensor(([[0.88, 0.12], [0.51, 0.49]], [[0.88, 0.12], [0.51, 0.49]], [[0.88, 0.12], [0.51, 0.49]]), dtype=torch.float)
+    y_pred = torch.tensor(([[0.88, 0.12], [0.51, 0.49]], [[0.88, 0.12], [0.51, 0.49]], [[0.88, 0.12], [0.51, 0.49]]),
+                          dtype=torch.float)
     y_true = torch.tensor([[1, 0], [1, 0], [1, 0]])
 
     # y_pred.requires_grad = True
     loss_fn = NllLoss()
-    my_loss = loss_fn(y_true, y_pred)
+    log_prob_y_pred = F.log_softmax(y_pred, 2)
+    my_loss = loss_fn(log_prob_y_pred, y_pred)
     print(my_loss)
     prob_y_pred = F.softmax(y_pred, 2)
 
