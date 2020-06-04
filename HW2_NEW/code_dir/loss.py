d035fad3d73622d6e0614c689910f6a2fdb99038
micha <miken081990@gmail.com>
2020-06-04 11:51:37 +0300

    loss same as library

--- HW2_NEW/code_dir/loss.py
+++ HW2_NEW/code_dir/loss.py
@@ -7,7 +7,7 @@ class NllLoss(torch.nn.Module):
     def __init__(self):
         super(NllLoss, self).__init__()
 
-    def forward(self, log_prob_y_pred, y_pred) -> torch.Tensor:
+    def forward(self, log_prob_y_pred, y_true) -> torch.Tensor:
         """
 
         :param y_true:
@@ -24,17 +24,17 @@ if __name__ == '__main__':
     # test loss with 3d tensor
     y_pred = torch.tensor(([[0.88, 0.12], [0.51, 0.49]], [[0.88, 0.12], [0.51, 0.49]], [[0.88, 0.12], [0.51, 0.49]]),
                           dtype=torch.float)
-    y_true = torch.tensor([[1, 0], [1, 0], [1, 0]])
+    y_true_out = torch.tensor([[1, 0], [1, 0], [1, 0]])
 
     # y_pred.requires_grad = True
     loss_fn = NllLoss()
-    log_prob_y_pred = F.log_softmax(y_pred, 2)
-    my_loss = loss_fn(log_prob_y_pred, y_pred)
+    log_prob_y_pred_out = F.log_softmax(y_pred, 2)
+    my_loss = loss_fn(log_prob_y_pred_out, y_true_out)
     print(my_loss)
     prob_y_pred = F.softmax(y_pred, 2)
 
-    loss_fn = torch.nn.NLLLoss(reduction='mean')
-    lib_loss = loss_fn(torch.log(prob_y_pred), y_true)
+    loss_fn = torch.nn.NLLLoss()
+    lib_loss = loss_fn(torch.log(prob_y_pred), y_true_out)
     print(lib_loss)
     assert lib_loss == my_loss
 
