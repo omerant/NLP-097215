fix accuracy calculation

HW2_NEW/code_dir/data_handling.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 import torch
 from utils import split, get_vocabs, get_vocabs_dep_parser, WORD_IDX_IN_LINE, POS_IDX_IN_LINE, HEAD_IDX_IN_LINE, IGNORE_IDX
-from torchtext.vocab import Vocab
+# from torchtext.vocab import Vocab
 from torch.utils.data.dataset import Dataset, TensorDataset
 from torch.utils.data.dataloader import DataLoader
 from pathlib import Path
@@ -223,7 +223,7 @@ class DepDataset(Dataset):
             pos_idx_list = []
             head_idx_list = []
             for word, pos, head in sentence:
-                dropout_prob = 0#ALPHA_DROPOUT/(self.word_dict[word] + ALPHA_DROPOUT)
+                dropout_prob = ALPHA_DROPOUT/(self.word_dict[word] + ALPHA_DROPOUT) if word != ROOT_TOKEN else 0
                 # print(f'dropout_prob: {dropout_prob}')
                 # print(f'count: {self.word_dict[word]}')
                 words_dropout_list.append(dropout_prob)

HW2_NEW/code_dir/model.py
@@ -35,32 +35,28 @@ class DnnPosTagger(nn.Module):
 
 
 class DnnSepParser(nn.Module):
-    def __init__(self, word_emb_dim, tag_emb_dim, num_layers, word_vocab_size, tag_vocab_size, max_sentence_len):
+    def __init__(self, word_emb_dim, tag_emb_dim, num_layers, word_vocab_size, tag_vocab_size):
         """
         :param word_emb_dim: dimension of word embedding
         :param tag_emb_dim: dimension of tag embedding
         :param word_vocab_size: used to create word embeddings
         :param num_layers: number of stack layers in LSTM
         :param tag_vocab_size: used to create tag embeddings
-        :param max_sentence_len: used to determine the output size of MLP
         """
         super(DnnSepParser, self).__init__()
-        # self.word_count = train_word_count_dict
         self.word_emb_dim = word_emb_dim
         self.tag_emb_dim = tag_emb_dim
-        self.hidden_dim = self.word_emb_dim + self.tag_emb_dim
+        self.hidden_dim_lstm = self.word_emb_dim + self.tag_emb_dim
         self.num_layers = num_layers
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.word_embedding = nn.Embedding(word_vocab_size, word_emb_dim)
         self.tag_embedding = nn.Embedding(tag_vocab_size, tag_emb_dim)
-        self.encoder = nn.LSTM(input_size=word_emb_dim + tag_emb_dim, hidden_size=self.hidden_dim, num_layers=num_layers,
+        self.encoder = nn.LSTM(input_size=word_emb_dim + tag_emb_dim, hidden_size=self.hidden_dim_lstm, num_layers=num_layers,
                                bidirectional=True, batch_first=False)
-        # self.hidden2first_mlp = nn.Linear(self.hidden_dim * 2, max_sentence_len)
-        # self.tanh = torch.nn.Tanh()
-        # self.first_mlp2second_mlp = nn.Linear(max_sentence_len, max_sentence_len)
-        self.tmp1 = nn.Linear(self.hidden_dim * 4, 100)
-        self.tmp_tan = nn.Tanh()
-        self.tmp2 = nn.Linear(100, 1)
+
+        self.fc1 = nn.Linear(self.hidden_dim_lstm * 4, 100)
+        self.tan = nn.Tanh()
+        self.fc2 = nn.Linear(100, 1)
         self.name = 'DnnDepParser' + '_' + 'word_emb-' + str(self.word_emb_dim) + '_' + 'tag_emb-' + str(self.tag_emb_dim) \
                     + '_' + 'num_stack' + str(self.num_layers)
 
@@ -72,8 +68,8 @@ class DnnSepParser(nn.Module):
         lstm_out, _ = self.encoder(concat_emb.view(concat_emb.shape[1], 1, -1))  # [seq_length, batch_size, 2*hidden_dim]
 
         lstm_out_b_first = lstm_out.permute(1, 0, 2)
-        first_part_out = (lstm_out_b_first @ self.tmp1.weight.T[:lstm_out_b_first.shape[2], :] + self.tmp1.bias.T).squeeze(0)
-        second_part_out = (lstm_out_b_first @ self.tmp1.weight.T[lstm_out_b_first.shape[2]:, :] + self.tmp1.bias.T).squeeze(0)
+        first_part_out = (lstm_out_b_first @ self.fc1.weight.T[:lstm_out_b_first.shape[2], :] + self.fc1.bias.T).squeeze(0)
+        second_part_out = (lstm_out_b_first @ self.fc1.weight.T[lstm_out_b_first.shape[2]:, :] + self.fc1.bias.T).squeeze(0)
         first_part_out1 = first_part_out.unsqueeze(0)
         second_part_out1 = second_part_out.unsqueeze(1)
         first_part_out2 = first_part_out1.repeat(second_part_out.shape[0], 1, 1)
@@ -81,7 +77,7 @@ class DnnSepParser(nn.Module):
         Z = first_part_out2 + second_part_out2
         out_1 = Z.view(-1, Z.shape[-1])# [seq_length**2,hidden_dim_mlp]
 
-        scores = self.tmp2(self.tmp_tan(out_1)).view(lstm_out.shape[0], lstm_out.shape[0]).squeeze(0)
+        scores = self.fc2(self.tan(out_1)).view(lstm_out.shape[0], lstm_out.shape[0]).squeeze(0)
         tmp_scores = F.log_softmax(scores, dim=1)
         # calc tree
         our_heads = None

HW2_NEW/code_dir/trainer.py
@@ -93,6 +93,7 @@ class Trainer:
         for epoch in range(1, num_epochs + 1):
             self.model.train()  # put in training mode
             running_epoch_loss = 0.0
+            running_epoch_acc = []
             epoch_time = time.time()
             i = 0
             for data in dl_train:
@@ -105,8 +106,18 @@ class Trainer:
                 words_idx_tensor[bern_distribution.sample().bool()] = UNK_IDX
                 # print(f'UNK_IDX: {UNK_IDX}')
                 # print(f'words_idx_tensor: {words_idx_tensor}')
-                dep_scores, _ = self.model(words_idx_tensor, pos_idx_tensor)
+                dep_scores, our_heads = self.model(words_idx_tensor, pos_idx_tensor, True)
                 dep_scores = dep_scores.unsqueeze(0).permute(0, 2, 1)
+
+                dep_idx_tensor_2d = dep_idx_tensor.squeeze(0)
+                running_epoch_acc += [np.mean(dep_idx_tensor_2d.numpy()[1:] == our_heads[1:])]
+                # cur_loss = loss_fn(dep_scores, dep_idx_tensor.to(device))
+
+
+
+                # dep_idx_tensor_2d = dep_idx_tensor.squeeze(0)
+                # running_epoch_acc += [np.mean(dep_idx_tensor_2d.numpy()[1:] == our_heads[1:])]
+
                 loss = self.loss_fn(dep_scores, dep_idx_tensor.to(self.device))
                 loss = loss / acumulate_grad_steps
                 loss.backward()
@@ -118,19 +129,18 @@ class Trainer:
             # Normalizing the loss by the total number of train batches
             running_epoch_loss /= (len_train/acumulate_grad_steps)
             train_loss_list.append(running_epoch_loss)
-            # Calculate training/test set accuracy of the existing model
-            # train_accuracy, _ = self.calculate_accuracy_dep_parser(self.model, dl_train, len_train, self.loss_fn,
-            #                                                        self.device, acumulate_grad_steps)
-            train_accuracy = 0.
-            train_acc_list.append(train_accuracy)
+            # print(f'running_epoch_acc: {running_epoch_acc}')
+            cur_epoch_train_accuracy = np.mean(running_epoch_acc) * 100
+            train_acc_list.append(cur_epoch_train_accuracy)
             cur_epoch_val_accuracy, cur_epoch_val_loss = self.calculate_accuracy_dep_parser(self.model, dl_val,
                                                                                             len_test, self.loss_fn,
                                                                                             self.device, acumulate_grad_steps)
+
             val_loss_list.append(cur_epoch_val_loss)
             val_acc_list.append(cur_epoch_val_accuracy)
             log = "Epoch: {} | Training Loss: {:.4f} | Training accuracy: {:.3f}% | Test Loss: {:.4f} | Test accuracy: {:.3f}% | ".format(epoch,
                                                                                                              running_epoch_loss,
-                                                                                                             train_accuracy,
+                                                                                                             cur_epoch_train_accuracy,
                                                                                                              cur_epoch_val_loss,
                                                                                                              cur_epoch_val_accuracy)
             epoch_time = time.time() - epoch_time
@@ -159,7 +169,8 @@ class Trainer:
                     'train_acc_list': train_acc_list
                 }
                 print('saving model')
-                torch.save(state, 'checkpoints/' + self.model.name + '.pth')
+                # insert more parameters to save
+                torch.save(state, 'checkpoints/' + self.model.name + '_' + str(acumulate_grad_steps) + '.pth')
 
         print('==> Finished Training ...')
 
@@ -188,18 +199,21 @@ class Trainer:
             for batch_idx, input_data in enumerate(dataloader):
                 if batch_idx % non_skip_idx == 0:
                     word_dropout_prob, words_idx_tensor, pos_idx_tensor, dep_idx_tensor, sentence_length = input_data
+
+                    # print(f'UNK_IDX: {UNK_IDX}')
+                    # print(f'words_idx_tensor: {words_idx_tensor}')
                     dep_scores, our_heads = model(words_idx_tensor, pos_idx_tensor, calc_mst=True)
                     assert our_heads is not None
                     dep_scores = dep_scores.unsqueeze(0).permute(0, 2, 1)
                     dep_idx_tensor_2d = dep_idx_tensor.squeeze(0)
-                    acc_list += [np.mean(dep_idx_tensor_2d.numpy() == our_heads)]
+                    acc_list += [np.mean(dep_idx_tensor_2d.numpy()[1:] == our_heads[1:])]
                     cur_loss = loss_fn(dep_scores, dep_idx_tensor.to(device))
-                    loss_list.append(cur_loss.cpu().numpy())
+                    loss_list.append(cur_loss.cpu().numpy()/acumulate_grad_steps)
 
             acc = np.mean(acc_list)
             acc *= 100
             # print(f'acc list: P{acc_list}')
-            loss = np.mean(loss_list)
+            loss = np.mean(loss_list) * acumulate_grad_steps
         return acc, loss
 
 