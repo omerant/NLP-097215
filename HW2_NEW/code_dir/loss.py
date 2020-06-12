e1d9a5650eb6956d98a272d498d1edf17cdb2017
micha <miken081990@gmail.com>
2020-06-12 19:26:17 +0300

    add hinge loss

--- HW2_NEW/code_dir/loss.py
+++ HW2_NEW/code_dir/loss.py
@@ -20,20 +20,49 @@ class NllLoss(torch.nn.Module):
         return our_loss
 
 
+class HingeLoss(torch.nn.Module):
+
+    def __init__(self):
+        super(HingeLoss, self).__init__()
+
+    def forward(self, y_pred, y_true) -> torch.Tensor:
+        """
+
+        :param y_true: 2d tensor
+        :param y_pred: 1d tensor
+        :return:
+        """
+        y_pred = y_pred.squeeze(0)
+        y_true = y_true.squeeze(0)
+        mask = torch.ones(y_pred.shape, dtype=torch.bool)
+        mask[range(y_true.shape[0]), y_true] = False
+        res = torch.max((1. - y_pred[mask].view(y_pred.shape[0], -1).max(dim=1).values + y_pred[range(y_true.shape[0]), y_true]).float())
+        return res
+        # Y_i_size = y_true.shape[1]
+        # our_loss = -(1. / (Y_i_size * y_true.shape[0])) * torch.sum(log_prob_y_pred.gather(1, y_true.unsqueeze(1)))
+        # return our_loss
+
+
 if __name__ == '__main__':
 
-    # test loss with 3d tensor
-    y_pred = torch.tensor(([[0.88, 0.12], [0.51, 0.49]], [[0.88, 0.12], [0.51, 0.49]], [[0.88, 0.12], [0.51, 0.49]]),
-                          dtype=torch.float)
-    y_true_out = torch.tensor([[1, 0], [1, 0], [1, 0]])
+    # # test loss with 3d tensor
+    # y_pred = torch.tensor(([[0.88, 0.12], [0.51, 0.49]], [[0.88, 0.12], [0.51, 0.49]], [[0.88, 0.12], [0.51, 0.49]]),
+    #                       dtype=torch.float)
+    # y_true_out = torch.tensor([[1, 0], [1, 0], [1, 0]])
+    #
+    # # y_pred.requires_grad = True
+    # loss_fn = NllLoss()
+    # log_prob_y_pred_out = F.log_softmax(y_pred, 2)
+    # my_loss = loss_fn(log_prob_y_pred_out[:, :, 1:], y_true_out[:, 1:])
+    # print(my_loss)
+    # # tensor(0.5718)
+    # loss_fn = NLLLoss()
+    # lib_loss = loss_fn(log_prob_y_pred_out[:, :, 1:], y_true_out[:, 1:])#loss_fn(log_prob_y_pred_out, y_true_out)
+    # print(lib_loss)
+    # assert lib_loss == my_loss
 
-    # y_pred.requires_grad = True
-    loss_fn = NllLoss()
-    log_prob_y_pred_out = F.log_softmax(y_pred, 2)
-    my_loss = loss_fn(log_prob_y_pred_out[:, :, 1:], y_true_out[:, 1:])
-    print(my_loss)
-    # tensor(0.5718)
-    loss_fn = NLLLoss()
-    lib_loss = loss_fn(log_prob_y_pred_out[:, :, 1:], y_true_out[:, 1:])#loss_fn(log_prob_y_pred_out, y_true_out)
-    print(lib_loss)
-    assert lib_loss == my_loss
+    y_pred = torch.tensor([[1, 2, 3], [2, 1, 17]]).unsqueeze(0)
+    y_true = torch.tensor([0, 0]).unsqueeze(0)
+    h_loss = HingeLoss()
+    loss = h_loss(y_pred, y_true)
+    print(loss)
